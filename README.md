2e793e2c30cb182a5d3d9a53417cb4d1e5d7b8ef
Isabelly Dias <isabellydias29@gmail.com>
2021-10-08 23:41:24 -0300

    Readme e rota estatica

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ac4-mensageria-rabbit
+
+* Este é uma atividade da matéria de arquitetura de sistemas do 4 semestre da faculdade impacta
+* antes de rodar os dois sistemas deve entrar na raiz de cada projeto onde se encontra o package.json e rodar o comando ```npm install```
+* Após só rodar ```npm run dev``` em cada pasta
+* para enviar uma mensagem é só rodar http://localhost:3001/enviar no postman ou insomninia com o método post
--- api-rabbit-producer/src/index.js
+++ api-rabbit-producer/src/index.js
@@ -1,7 +1,20 @@
-const customExpress = require('./config/expressConfig');
+const express = require('express');
 
-const app = customExpress();
+const Cliente = require("./models/cliente");
+const Produto = require("./models/produto");
+const Venda = require("./models/venda");
+const enviarVenda = require("./rabbitmq/send");
+
+const app = express();
 
 const PORT = 3001;
+
+app.post('/enviar', (req, res) => {
+    let cliente = new Cliente(1, 'Fulano da Silva', 'fulano@gmail.com', Date.now());
+    let produto = new Produto(1, 'Celular xpto', 'eletronicos', Date.now(), 1000.00);
+    let venda = new Venda(cliente, produto, 5);
+    enviarVenda(venda);
+    res.send("ENVIANDO A VENDA");
+    });
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
